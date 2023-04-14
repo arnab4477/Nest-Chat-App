cd71e4796a92df5ebc927383067fc6e1954db32f
arnab4477 <sahaarnabsw@gmail.com>
2023-04-14 19:49:29 +0530

    Added Swagger to the project for documentations

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Nest Real Time Chat API')
+    .setDescription(
+      'This is a Nest server for a real time chat application created with Nestjs, socket.io, postgres and TypeORM',
+    )
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('/docs', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
